08fcd17f274a2d3966869a7d064458274121ed19
OwenTho <118353507+OwenTho@users.noreply.github.com>
2024-07-13 03:20:11 +0100

    Simple Main Menu
    Added a simple main menu for the game so that I can add a "Start" button to initialise a game.

---
+++ scripts/menu/main_menu.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+func _on_btn_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_quit_pressed() -> void:
+	get_tree().quit()
+
---
+++ scripts/rules/piece/base/PawnMoveRule.cs
@@ -0,0 +1,27 @@
+﻿using Godot;
+using System.Collections.Generic;
+
+internal partial class PawnMoveRule : RuleBase
+{
+    public override List<Vector2I> GetPossibleMoves(Piece piece)
+    {
+        int maxForward = piece.info.level;
+        if (piece.timesMoved == 0)
+        {
+            maxForward += 1;
+        }
+
+        List<Vector2I> possibleMoves = new List<Vector2I>();
+        Vector2I thisPosition = new Vector2I(piece.cell.x, piece.cell.y);
+        for (int i = 0; i < maxForward; i++)
+        {
+            possibleMoves.Add(thisPosition + (piece.info.forwardDirection * i));
+        }
+        return possibleMoves;
+    }
+
+    public override List<Vector2I> GetPossibleAttacks(Piece piece)
+    {
+        throw new System.NotImplementedException();
+    }
+}
