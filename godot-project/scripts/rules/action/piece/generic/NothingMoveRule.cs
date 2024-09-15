2b106616f901e45ae98872ae512c2a01cc2856a0
OwenTho <118353507+OwenTho@users.noreply.github.com>
2024-09-15 23:32:56 +0100

    Create NothingMoveRule.cs

---
+++ godot-project/scripts/rules/action/piece/generic/NothingMoveRule.cs
@@ -0,0 +1,8 @@
+﻿
+public partial class NothingMoveRule : ActionRuleBase
+{
+    public override void AddPossibleActions(GameState game, Piece piece, int level)
+    {
+        piece.AddAction(new NothingAction(piece, piece.cell.pos));
+    }
+}
