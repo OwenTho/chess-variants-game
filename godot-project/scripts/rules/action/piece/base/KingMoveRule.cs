c28f4c31136417beedee1bb2b9f02ea47e5c7116
OwenTho <118353507+OwenTho@users.noreply.github.com>
2024-09-17 20:15:16 +0100

    King level up
    The King now levels up similarly to Rooks, with the additional ability to move and attack diagonally one space.

--- godot-project/scripts/rules/action/piece/base/KingMoveRule.cs
+++ godot-project/scripts/rules/action/piece/base/KingMoveRule.cs
@@ -2,20 +2,29 @@
 using Godot.Collections;
 
 
-internal partial class KingMoveRule : ActionRuleBase
+internal partial class KingMoveRule : LineMoveRule
 {
     public override void AddPossibleActions(GameState game, Piece piece, int level)
     {
+        base.AddPossibleActions(game, piece, level);
+        // In addition to the LineMove, also allow a single diagonal move
+        // This results in the King being a slightly better Rook.
         Vector2I thisPosition = piece.cell.pos;
         Attack(piece, thisPosition + GridVectors.UpLeft, AttackType.IfMove); // Up Left
-        Attack(piece, thisPosition + GridVectors.Up, AttackType.IfMove); // Up
         Attack(piece, thisPosition + GridVectors.UpRight, AttackType.IfMove); // Up Right
 
-        Attack(piece, thisPosition + GridVectors.Left, AttackType.IfMove); // Left
-        Attack(piece, thisPosition + GridVectors.Right, AttackType.IfMove); // Right
-
         Attack(piece, thisPosition + GridVectors.DownLeft, AttackType.IfMove); // Down Left
-        Attack(piece, thisPosition + GridVectors.Down, AttackType.IfMove); // Down
         Attack(piece, thisPosition + GridVectors.DownRight, AttackType.IfMove); // Down Right
+    }
+
+    public override Vector2I[] GetDirs(GameState game, Piece piece)
+    {
+        return new[]
+        {
+            GridVectors.Up,
+            GridVectors.Left,
+            GridVectors.Right,
+            GridVectors.Down
+        };
     }
 }
