09259b85743aaf09b0101009ebf8028eaf1bf82c
OwenTho <118353507+OwenTho@users.noreply.github.com>
2024-09-23 05:01:12 +0100

    Update CardWeights.cs

--- godot-project/scripts/game/card/CardWeights.cs
+++ godot-project/scripts/game/card/CardWeights.cs
@@ -2,7 +2,11 @@
 public static class CardWeights
 {
     public const int Common = 100;
+    public const int SlightlyUncommon = 100;
     public const int Uncommon = 60;
-    public const int Rare = 30;
+    public const int VeryUncommon = 40;
+    public const int SlightlyRare = 30;
+    public const int Rare = 20;
     public const int VeryRare = 10;
+    public const int ExtremelyRare = 2;
 }
