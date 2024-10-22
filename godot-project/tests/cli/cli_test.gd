17c34829ba260dd0f124e0a1c4efe2d67ffdf282
OwenTho <118353507+OwenTho@users.noreply.github.com>
2024-10-22 01:18:49 +0100

    Update godot-tester.yml
    Fix to result-output-file.

--- godot-project/tests/cli/cli_test.gd
+++ godot-project/tests/cli/cli_test.gd
@@ -4,7 +4,7 @@ func _ready():
 	var template_file = FileAccess.open("res://tests/scene_test_output.xml", FileAccess.READ)
 	var content = template_file.get_as_text()
 	
-	var output_file = FileAccess.open("res://direct_scene_xml_output.xml", FileAccess.WRITE)
+	var output_file = FileAccess.open("res://test_results.xml", FileAccess.WRITE)
 	
 	template_file.close()
 	output_file.close()
